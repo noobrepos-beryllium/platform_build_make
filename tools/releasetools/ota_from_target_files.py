55015cebf86b7ed68d320ec76d9eac39e6074235
RucioTozzy <fallgray@gmail.com>
2021-02-19 01:53:09 +0800

    ota_from_target_files: Don't show maintainer info when prop is null

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -821,14 +821,16 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   # Dump fingerprints
   script.Print("Target: {}".format(target_info.fingerprint))
-  
+
   android_version = target_info.GetBuildProp("ro.build.version.release")
   build_id = target_info.GetBuildProp("ro.build.id")
   build_date = target_info.GetBuildProp("ro.build.date")
   security_patch = target_info.GetBuildProp("ro.build.version.security_patch")
   device = target_info.GetBuildProp("ro.octavi.device")
-  octavi_maintainer = target_info.GetBuildProp("ro.maintainer.name")
-  
+  octavi_maintainer = target_info.GetBuildProp("ro.octavi.maintainer").strip()
+  if not octavi_maintainer:
+      octavi_maintainer = ''
+
   script.Print("--------------------------------------------------");
   script.Print("   ____  _______________ _    ______   ____  _____");
   script.Print("  / __ \/ ____/_  __/   | |  / /  _/  / __ \/ ___/");
@@ -842,7 +844,8 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print(" Build date: %s"%(build_date));
   script.Print(" Security patch: %s"%(security_patch));
   script.Print(" Device: %s"%(device));
-  script.Print(" Maintainer: %s"%(octavi_maintainer));
+  if octavi_maintainer != '':
+      script.Print(" Maintainer: %s"%(octavi_maintainer));
   script.Print("--------------------------------------------------");
 
   device_specific.FullOTA_InstallBegin()
